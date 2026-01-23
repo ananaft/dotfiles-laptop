c2a23eedcdb1e6c02b82fbb4c0f5651ae348afbd
ananaft <lukas.hoettges@gmx.de>
2026-01-23 10:53:18 +0100

    Add C treesitter to nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,6 @@ require('nvim-treesitter').setup {
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = { 
 		'bash', 'css', 'html', 'javascript', 'json', 'latex', 'lua',
-		'markdown', 'python', 'typescript'
+		'markdown', 'python', 'typescript', 'c'
 	}
 }
