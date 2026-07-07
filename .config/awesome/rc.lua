170ab3337113a79ec43bf16976975151abf6cd30
ananaft <lukas.hoettges@gmx.de>
2026-07-07 11:27:47 +0200

    Awesome: fix firefox fullscreen offset

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -572,8 +572,8 @@ awful.rules.rules = {
       }, properties = { floating = true }},
 
     -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+    { rule_any = { type = "dialog" },
+		properties = { titlebars_enabled = true }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
